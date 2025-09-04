e6d395ed379efb8a94c222a1d052159fb48a1043
rodrin7 <rodrigo.nudi@alumnos.upm.es>
2025-09-04 12:56:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,21 +14,22 @@ client = Groq(api_key=os.getenv('GROQ_API_KEY'))
 
 @app.route('/', methods=['GET'])
 def index():
-    # Define genres in Spanish
-    genres = [
-        'Ciencia Ficción', 'Fantasía', 'Misterio', 'Romance', 
-        'Ficción Histórica', 'Ficción Literaria', 'Terror',
-        'Suspense', 'No Ficción', 'Biografía'
+    # Edad
+    age = [
+    '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33',
+    '34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64',
+    '65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95',
+    '96','97','98','99'
     ]
-    return render_template('index.html', genres=genres)
+    return render_template('index.html', age=age)
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
     # Get form data
-    genre = request.form.get('genre')
+    age = request.form.get('age')
     author = request.form.get('author')
     recent_books = request.form.get('recent_books')
-    reading_level = request.form.get('reading_level')
+    reading_level = request.form.get('activity_level')
 
     # Reading level translation
     reading_level_es = {
