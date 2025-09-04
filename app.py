3396a8adbc6c71ea42dde6ec85616298542d251e
rodrin7 <rodrigo.nudi@alumnos.upm.es>
2025-09-04 13:21:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
     '65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95',
     '96','97','98','99'
     ]
-    return render_template('index.html', edad=edad)
+    return render_template('index.html', edades=edades)
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
